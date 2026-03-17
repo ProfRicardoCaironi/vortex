559f9a3df248f354a491d74d819606b32f338e17
ProfRicardoCaironi <prof.ricardo.caironi.senai404@gmail.com>
2026-03-17 16:28:57 -0300

    Meu primeiro commit

---
+++ src/data/games.js
@@ -0,0 +1,19 @@
+import league from "../assets/league.jpg";
+import valorant from "../assets/valorant.jpg";
+import minecraft from "../assets/minecraft.jpg";
+import cs from "../assets/cs2.jpg";
+import cyberpunk from "../assets/cyberpunk.png";
+import elden from "../assets/elden.jpg";
+import fifa from "../assets/fifa.webp";
+import apex from "../assets/apex.jpg";
+
+export const gamesData = [
+    { id: 1, title: "League of Legends", category: "MOBA", banner: league },
+    { id: 2, title: "Valorant", category: "FPS", banner: valorant },
+    { id: 3, title: "Minecraft", category: "Survival", banner: minecraft },
+    { id: 4, title: "CS:GO 2", category: "FPS", banner:cs },
+    { id: 5, title: "Cyberpunk 2077", category: "RPG", banner: cyberpunk },
+    { id: 6, title: "Elden Ring", category: "Soulslike", banner: elden },
+    { id: 7, title: "FIFA 24", category: "Esportes", banner: fifa },
+    { id: 8, title: "Apex Legends", category: "Battle Royale", banner: apex },
+  ];
